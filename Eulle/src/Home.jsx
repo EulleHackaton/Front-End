9c383dd1558ae23cc2bd8d21b265ffeed4912fea
Flepsz <luisfelipelfsp3@gmail.com>
2023-07-05 08:16:07 -0300

    Testing

---
+++ Eulle/src/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css"
+
+function Home() {
+    return(
+        <main>
+            <h1>
+                Oi Bom?
+            </h1>
+        </main>
+    )
+};
+
+export default Home;
